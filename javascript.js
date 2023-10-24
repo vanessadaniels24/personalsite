6d29e56a6d1e2ac86401ea70f6e1a9d1f7303284
vanessadaniels24 <vanessadaniels24@gmail.com>
2023-10-24 14:08:47 -0400

    commit code

---
+++ javascript.js
@@ -0,0 +1,7 @@
+<colorsdivi></colorsdivi>"colors"
+const colorsdiv = document.getElementById(elementld' colors ');
+
+const p = document.getElementById(elementld'my_p');
+
+p.innerText= 'Hello world'
+
